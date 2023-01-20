4450a677c55cf8e026d879220804a1e4d375561e
Vinicius <viniciusflexa@gmail.com>
2023-01-20 13:22:04 -0300

    pop up habit day update with API

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -1,21 +1,19 @@
 interface ProgressBarProps {
     progress: number
-}
-
-export function ProgressBar(props: ProgressBarProps) {
-    const progressStyles = {
-        width: `${props.progress}%`
-    }
-
-    return(
-        <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
-            <div 
-                role="progressbar"
-                aria-label="Progresso de hábitos completados nesse dia"
-                aria-aria-valuenow={props.progress}
-                className='h-3 rounded-xl bg-violet-600 w-3/4' 
-                style={progressStyles}
-            />
-        </div>        
+  }
+  
+  export function ProgressBar(props: ProgressBarProps) {
+    return (
+      <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+        <div 
+          role="progressbar"
+          aria-label="Progresso de hábitos completados nesse dia"
+          aria-valuenow={props.progress}
+          className="h-3 rounded-xl bg-violet-600"
+          style={{
+            width: `${props.progress}%`
+          }}
+        />
+      </div>
     )
-}
+  }
