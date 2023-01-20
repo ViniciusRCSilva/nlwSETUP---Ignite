76c90f91d2812bf503c782ae20c8b1474b9c026c
Vinicius <viniciusflexa@gmail.com>
2023-01-20 12:43:57 -0300

    correction get day

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -54,7 +54,7 @@ export async function appRoutes(app: FastifyInstance) {
             }
         })
 
-        const day = await prisma.day.findUnique({
+        const day = await prisma.day.findFirst({
             where: {
                 date: parsedDate.toDate()
             },
