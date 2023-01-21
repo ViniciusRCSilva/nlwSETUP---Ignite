59c412e81331cdbf67df6cf2b3a5ae5c3e936440
Vinicius <viniciusflexa@gmail.com>
2023-01-21 12:04:45 -0300

    post correction

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,30 +6,30 @@ import { prisma } from './lib/prisma'
 export async function appRoutes(app: FastifyInstance) {
     app.post('/habits', async (request) => {
         const createHabitBody = z.object({
-            title: z.string(),
-            weekDays: z.array(
-                z.number().min(0).max(6)
-            )
+          title: z.string(),
+          weekDays: z.array(
+            z.number().min(0).max(6)
+          ),
         })
-        
+    
         const { title, weekDays } = createHabitBody.parse(request.body)
-
+    
         const today = dayjs().startOf('day').toDate()
-
+    
         await prisma.habit.create({
-            data: {
-                title,
-                create_at: new Date(),
-                weekDays: {
-                    create: weekDays.map(weekDays => {
-                        return {
-                            week_day: weekDays
-                        }
-                    })
+          data: {
+            title,
+            create_at: today,
+            weekDays: {
+              create: weekDays.map((weekDay) => {
+                return {
+                  week_day: weekDay,
                 }
+              }),
             }
+          }
         })
-    })
+      })
 
     app.get('/day', async (request) => {
         const getDayParams = z.object({
